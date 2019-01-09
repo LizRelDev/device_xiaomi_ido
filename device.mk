41eba7118280e8381ff8027c77cf3f400777259c
shoudil <stephenli@codeaurora.org>
2019-01-09 17:04:11 +0800

    ido: Add framework detect modules to PRODUCT_PACKAGES
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: Ic64f5ac0cfbbe9e105a6b48b5bc7687c8d23d7e8

--- device.mk
+++ device.mk
@@ -133,6 +133,10 @@ PRODUCT_PACKAGES += \
     libfmjni \
     FMRadio
 
+# Fwk-detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect
+
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
